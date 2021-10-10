ae8b6aea9d826f979e46a5e90b6aaa7afd60126b
- <c0812652@mylambton.ca>
2021-10-11 01:36:56 +0530

    screen layout set

---
+++ app/src/main/java/com/example/note_rmgs_android/Add_Edit_Note.java
@@ -0,0 +1,14 @@
+package com.example.note_rmgs_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Edit_Note extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_note);
+    }
+}
