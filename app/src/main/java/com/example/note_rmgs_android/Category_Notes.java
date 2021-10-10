8ff454a90bb83f31d9e0744ab2c11ef77bdbe029
- <c0812652@mylambton.ca>
2021-10-11 00:33:08 +0530

    Create category notes activity

---
+++ app/src/main/java/com/example/note_rmgs_android/Category_Notes.java
@@ -0,0 +1,14 @@
+package com.example.note_rmgs_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Category_Notes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_notes);
+    }
+}
