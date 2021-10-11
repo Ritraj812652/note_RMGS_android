bba8213d70e7941af2f9f66654e5c69b66a16436
- <C0823239@mylambton.ca>
2021-10-12 02:54:23 +0530

    Working on new note screen

--- app/src/main/java/com/example/note_rmgs_android/Add_Edit_Note.java
+++ app/src/main/java/com/example/note_rmgs_android/Add_Edit_Note.java
@@ -3,12 +3,26 @@ package com.example.note_rmgs_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Add_Edit_Note extends AppCompatActivity {
 
+    @BindView(R.id.txt_heading)
+    TextView txt_heading;
+    @BindView(R.id.img_left)
+    ImageView img_left;
+    @BindView(R.id.img_icon)
+    ImageView img_icon;
+    @BindView(R.id.img_right)
+    ImageView img_right;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_note);
+        ButterKnife.bind(this);
     }
 }
