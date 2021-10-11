ba675e2caf4b579f9a69c5a0e3d6fe4320926002
- <C0823239@mylambton.ca>
2021-10-12 03:04:07 +0530

    Binding views in Addnote Screen

--- app/src/main/java/com/example/note_rmgs_android/Add_Edit_Note.java
+++ app/src/main/java/com/example/note_rmgs_android/Add_Edit_Note.java
@@ -3,7 +3,10 @@ package com.example.note_rmgs_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -19,6 +22,24 @@ public class Add_Edit_Note extends AppCompatActivity {
     ImageView img_icon;
     @BindView(R.id.img_right)
     ImageView img_right;
+    @BindView(R.id.new_title)
+    EditText new_title;
+    @BindView(R.id.note_description)
+    EditText note_description;
+    @BindView(R.id.image_picker)
+    ImageView image_picker;
+    @BindView(R.id.voice_picker)
+    ImageView voice_picker;
+    @BindView(R.id.view_layout)
+    LinearLayout view_layout;
+    @BindView(R.id._pic)
+    ImageView _pic;
+    @BindView(R.id.play)
+    ImageView play;
+    @BindView(R.id.save)
+    Button save;
+    @BindView(R.id.select_subject)
+    TextView select_subject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
