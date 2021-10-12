bd0a04b8bee0ebda2d65b4bc6b6bb1e3217a3615
- <C0823239@mylambton.ca>
2021-10-12 23:44:44 +0530

    Worked on Image picker functionality

--- app/src/main/java/com/example/note_rmgs_android/Add_Edit_Note.java
+++ app/src/main/java/com/example/note_rmgs_android/Add_Edit_Note.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,6 +30,7 @@ import com.example.note_rmgs_android.Models.Note;
 import com.example.note_rmgs_android.Models.Subject;
 import com.example.note_rmgs_android.Utils.DataConverter;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
@@ -69,6 +74,8 @@ public class Add_Edit_Note extends AppCompatActivity {
     int id;
     Subject subjectdata;
     private static final int REQUEST_CODE = 1;
+    private static final int CAMERA_REQUEST = 102;
+    private static final int GALLERY_REQUEST = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -139,6 +146,7 @@ public class Add_Edit_Note extends AppCompatActivity {
                     note = new Note( new_title.getText().toString(),note_description.getText().toString(), userlocation.getLatitude(), userlocation.getLongitude(), null, pathAudio,new Date().getTime(),id );
                 }
                 Database.getInstance(this).noteDeo().insertNote(note);
+              finish();
 
             }
         }else {
@@ -155,6 +163,7 @@ public class Add_Edit_Note extends AppCompatActivity {
                         note.setImage(DataConverter.convertImage2ByteArray(image));
                     }
                     Database.getInstance(this).noteDeo().updateNote(note);
+                   finish();
                 }
 
             }
@@ -220,5 +229,97 @@ public class Add_Edit_Note extends AppCompatActivity {
         return true;
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+        if (reqCode == CAMERA_REQUEST && resultCode == RESULT_OK)
+        {
+            image = (Bitmap) data.getExtras().get("data");
+         _pic.setImageBitmap(image);
+           _pic.setVisibility(View.VISIBLE);
+
+        }
+        else if(reqCode == 112 && resultCode == RESULT_OK){
+
+        }
+        else if(reqCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+
+            Uri uri = data.getData();
+            try {
+                image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+               _pic.setImageBitmap(image);
+               _pic.setVisibility(View.VISIBLE);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        else if (reqCode == 11){
+            if(resultCode == RESULT_OK) {
+                int subjectID = data.getIntExtra("SubjectID",-1);
+                id=subjectID;
+                if(subjectID != -1){
+                    for (Subject sub: Database.getInstance(this).subjectDeo().getAllSubject()) {
+                        if(sub.getSubject_id() == subjectID){
+                            subjectdata = sub;
+                        }
+                    }
+
+                }
+            }
+        }
+        else{
+
+        }
+    }
+    public void gallery() {
+
+        final CharSequence[] items = { "Take Photo", "Choose from Library","Record Audio","Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose Your Option");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                //  boolean result= Utility.checkPermission(ShareDeal.this);
+
+                if (items[item].equals("Take Photo")) {
+
+                    CaptureImage();
+                } else if (items[item].equals("Choose from Library")) {
+                    OpenGallery();
+                } else if(items[item].equals("Record Audio")){
+
+                }
+                else if(items[item].equals("Cancel")){
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+
+    }
+
+    public void CaptureImage() {
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    public void OpenGallery() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+    }
+    private void selectImage() {
+        gallery();
+
+    }
+    @OnClick(R.id.image_picker)
+    public void getIamge()
+    {
+        selectImage();
     }
 }
