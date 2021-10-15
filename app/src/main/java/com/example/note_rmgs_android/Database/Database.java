6edb318c5fac19797110a511f1e82f0f7cb84a9a
Ritraj <c0812652@mylambton.ca>
2021-10-15 16:45:55 -0400

    Fallback commit

--- app/src/main/java/com/example/note_rmgs_android/Database/Database.java
+++ app/src/main/java/com/example/note_rmgs_android/Database/Database.java
@@ -16,6 +16,7 @@ public class Database {
             return db = Room.databaseBuilder(context,
                     Db.class,
                     "NotesDatabase")
+                    .fallbackToDestructiveMigration()
                     .allowMainThreadQueries().build();
         }
 
